2b77890c79039bc3292f3e25d0bfbb32922b0a71
Wei Zheng <ivan.zheng1201@gmail.com>
2018-04-04 19:36:54 -0400

    new file for encode decode without figure

--- rules.py
+++ rules.py
@@ -1,44 +1,7 @@
 # coding: utf-8
-from nltk.tag.stanford import StanfordPOSTagger
-st = StanfordPOSTagger('english-bidirectional-distsim.tagger')
+
 from nltk.parse.stanford import StanfordParser
 parser=StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-list(parser.raw_parse("the quick brown fox jumps over the lazy dog"))
-a=parser.raw_parse("the quick brown fox jumps over the lazy dog")
-a
-a=list(s)
-a=list(a)
-a
-a.productions()
-a[0].productions()
-a=parse.raw_parse("I love you")
-a=parser.raw_parse("I love you")
+a=list(parser.raw_parse("Please edit your question to be more clear about what you need"))[0]
+a=list(parser.raw_parse("Please edit your question to be more clear about what you need"))[0]
 print(a)
-list(a)[0]
-list(a)[0].productions()
-(list(a)[0]).productions()
-list(a)[0]
-a=parser.raw_parse("I love you")
-list(a)[0]
-(list(a)[0]).productions()
-list(a)[0]
-a=parser.raw_parse("I love you")
-a
-a=list(parser.raw_parse("I love you"))[0]
-a
-a.productions()
-a=list(parser.raw_parse("Please edit your question to be more clear about what you need"))[0]
-a.productions()
-sort(a.productions())
-sortd(a.productions())
-sorted(a.productions())
-a=list(parser.raw_parse("Please edit your question to be more clear about what you need"))[0]
-sorted(a.productions())
-get_ipython().magic(u'save stanfordnltk')
-get_ipython().magic(u'save stanfordnltk .')
-get_ipython().magic(u'save stanfordnltk .')
-get_ipython().magic(u'save stanfordnltk')
-get_ipython().magic(u'pinfo %save')
-get_ipython().magic(u'pwd ')
-get_ipython().magic(u'save getrules')
-get_ipython().magic(u'save getrules 1-999')
