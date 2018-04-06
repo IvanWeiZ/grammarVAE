70fda807e607ac97d224d670f67e992b0d9f09a4
Wei Zheng <ivan.zheng1201@gmail.com>
2018-04-06 02:34:58 -0400

    more messages for toy vae

--- toy_vae.py
+++ toy_vae.py
@@ -92,13 +92,19 @@ class ToyGrammarModel(object):
         print(unmasked.shape[1])
         # Loop over time axis, sampling values and updating masks
         for t in xrange(unmasked.shape[1]):
+            print('S')
+            print(S)
             next_nonterminal = [self._lhs_map[pop_or_nothing(a)] for a in S]
+            print('next_nonterminal')
             print(next_nonterminal)
             mask = self._grammar.masks[next_nonterminal]
+            print('mask')
             print(mask)
             masked_output = np.exp(unmasked[:,t,:])*mask + eps
+            print('masked_output')
             print(masked_output)
             sampled_output = np.argmax(np.random.gumbel(size=masked_output.shape) + np.log(masked_output), axis=-1)
+            print('sampled_output')
             print(sampled_output)
             X_hat[np.arange(unmasked.shape[0]),t,sampled_output] = 1.0
             # Identify non-terminals in RHS of selected production, and
@@ -106,6 +112,8 @@ class ToyGrammarModel(object):
             rhs = [filter(lambda a: (type(a) == nltk.grammar.Nonterminal) and (str(a) != 'None'),
                           self._productions[i].rhs()) 
                    for i in sampled_output]
+            print('rhs')
+            print('rhs')
             for ix in xrange(S.shape[0]):
                 S[ix].extend(map(str, rhs[ix])[::-1])
         return X_hat # , ln_p
