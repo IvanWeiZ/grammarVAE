b4963d3c41a0cc1ad5bbe7d7906d8411479c18a3
Wei Zheng <ivan.zheng1201@gmail.com>
2018-04-05 20:13:05 -0400

    make toy str dataset

--- make_toy_dataset_str.py
+++ make_toy_dataset_str.py
@@ -4,9 +4,10 @@ from models.utils import many_one_hot
 import h5py
 import argparse
 
-path='data/250k_rndm_zinc_drugs_clean.smi'
+path='data/toylanguage100k'
+
 def get_arguments():
-    parser = argparse.ArgumentParser(description='Make zinc str datset')
+    parser = argparse.ArgumentParser(description='Make toy datset')
     parser.add_argument('--path', type=str, default=path)
     return parser.parse_args()
 
@@ -16,7 +17,7 @@ print(args.path)
 f = open(args.path,'r')
 
 L = []
-chars = ['C', '(', ')', 'c', '1', '2', 'o', '=', 'O', 'N', '3', 'F', '[', '@', 'H', ']', 'n', '-', '#', 'S', 'l', '+', 's', 'B', 'r', '/', '4', '\\', '5', '6', '7', 'I', 'P', '8', ' ']
+chars = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 DIM = len(chars)
 for line in f:
     line = line.strip()
@@ -24,18 +25,18 @@ for line in f:
 f.close()
 
 count = 0
-MAX_LEN = 120
+MAX_LEN = 100
 OH = np.zeros((len(L),MAX_LEN,DIM))
-for chem in L:
+for line in L:
     indices = []
-    for c in chem:
+    for c in line:
         indices.append(chars.index(c))
     if len(indices) < MAX_LEN:
         indices.extend((MAX_LEN-len(indices))*[DIM-1])
     OH[count,:,:] = many_one_hot(np.array(indices), DIM)
     count = count + 1
 f.close()
-h5f = h5py.File('zinc_str_dataset.h5','w')
+h5f = h5py.File('toy_str_dataset.h5','w')
 h5f.create_dataset('data', data=OH)
 h5f.create_dataset('chr',  data=chars)
 h5f.close()
